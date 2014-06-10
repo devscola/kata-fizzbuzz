ad8fc8198a051c6f409b2356cab0dcdcf1cabc7d
Eduardo Sebastian <esebastian@altiora.es>
2014-06-10 18:56:31 +0200

    Refactor the solution by applying the previously used techniques, 'no magic numbers' and 'replace with query'

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,18 @@
 FIZZ = 'Fizz'
 FIZZ_NUMBER = 3
+BUZZ = 'Buzz'
+BUZZ_NUMBER = 5
 
 def fizzbuzz the_number
   return FIZZ if fizz_number? the_number
-  return 'Buzz' if the_number == 5
+  return BUZZ if buzz_number? the_number
   the_number
 end
 
 def fizz_number? the_number
   the_number == FIZZ_NUMBER
+end
+
+def buzz_number? the_number
+  the_number == BUZZ_NUMBER
 end
