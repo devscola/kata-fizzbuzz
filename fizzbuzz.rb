c0dd5c1b81a7b07f74691490cd4c2e1f2b7d58d2
Eduardo Sebastian <esebastian@altiora.es>
2014-06-10 18:49:08 +0200

    Refactor the solution by extracting the conditional expression to a query method

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,10 @@ FIZZ = 'Fizz'
 FIZZ_NUMBER = 3
 
 def fizzbuzz the_number
-  return FIZZ if the_number == FIZZ_NUMBER
+  return FIZZ if fizz_number? the_number
   the_number
+end
+
+def fizz_number? the_number
+  the_number == FIZZ_NUMBER
 end
