271afe4892fa8b02c1c3eb63ac40bfbe9c85a861
Eduardo Sebastian <esebastian@altiora.es>
2014-06-11 23:40:27 +0200

    Refactor by applying the 'collecting variable' technique as those aren't 'guard clauses' anymore

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,12 @@ BUZZ_NUMBER = 5
 NO_REMAINDER = 0
 
 def fizzbuzz the_number
-  return FIZZ + BUZZ if (fizz_number? the_number) && (buzz_number? the_number)
-  return FIZZ if fizz_number? the_number
-  return BUZZ if buzz_number? the_number
-  the_number
+  result = ''
+  result = the_number
+  result = FIZZ if fizz_number? the_number
+  result = BUZZ if buzz_number? the_number
+  result = FIZZ + BUZZ if (fizz_number? the_number) && (buzz_number? the_number)
+  result
 end
 
 def fizz_number? the_number
